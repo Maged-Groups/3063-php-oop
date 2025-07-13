49324b114818b46fced3e8b4efaa215616704345
Maged <magedyaseengroups@gmail.com>
2025-07-13 18:17:54 +0300

    Str class methods body created

--- app/Str.php
+++ app/Str.php
@@ -14,26 +14,31 @@ class Str
 
     public static function reverse($str)
     {
-
+        return strrev($str);
     }
 
     public static function upper($str)
     {
-
+        return strtoupper($str);
     }
 
     public static function lower($str)
     {
-
+        return strtolower($str);
     }
 
     public static function ucwords($str)
     {
-
+        return ucwords($str);
     }
 
     public static function contains($str, $substr)
     {
+        if (strpos($str, $substr) === false) {
+            return false;
+        }
+
+        return true;
 
     }
 
